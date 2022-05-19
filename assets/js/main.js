249d5f8b5fd530ece163cb8a018308f2d5bef6ea
ladudasha <darya.gameza@gmail.com>
2022-05-20 00:00:33 +0300

    js

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+// плавный скролл
+
+let anchors = document.querySelectorAll("a[href*='#']")
+// console.log(anchors);
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(event) {
+    event.preventDefault()
+    
+    let blockID  = anchor.getAttribute('href');
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+// показать баннер
+
+// let showBanner  = document.querySelector('.banner')
+
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 1500) {
+//     showBanner.style.display = 'block'
+//   } else {
+//     showBanner.style.display = 'none'
+//     showBanner.style.position = 'sticky'  
+//     showBanner.style.top = 0    
+//   }
+// })
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
